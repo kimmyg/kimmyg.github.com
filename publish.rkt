70420d029a0fb85b423abc54ed4dfbba5a44b2eb
Kimball Germane <krgermane@gmail.com>
2013-04-24 13:13:23 -0600

    Publishing.

---
+++ publish.rkt
@@ -0,0 +1,33 @@
+#lang racket/base
+(require racket/date
+         racket/match
+         ;racket/system
+         "render-post.rkt")
+
+(define system displayln)
+
+(with-handlers ([(λ (_) #f) (λ (e)
+                              (displayln "usage: racket publish.rkt technical|freedom name"))])
+  (match-let ([(vector strand name) (current-command-line-arguments)])
+    (define draft-path (format "~a/drafts/~a.txt" strand name))
+    (define hierarchy
+      (let ([now (current-date)])
+        (format "~a/~a/~a" (date-year now) (date-month now) (date-day now))))
+    (define final-path (format "~a/~a/~a.txt" strand hierarchy name))
+    (define post-path (format "~a/~a/~a.html" strand hierarchy name))
+    (if (file-exists? draft-path)
+        (begin
+          (system "git checkout source")
+          (system (format "mkdir -p ~a/~a" strand hierarchy))
+          (system (format "git mv ~a ~a" draft-path final-path))
+          (system (format "git commit -m \"Publish ~a.\"" name))
+          (let ([post (render-post final-path)])
+            (system "git checkout master")
+            (system (format "mkdir -p ~a/~a" strand hierarchy))
+            (with-output-to-file post-path
+              (λ () (write post)))
+            (system (format "git add ~a" post-path))
+            (system (format "git commit -m \"Publish ~a.\"" name))
+            (system "git push origin source")
+            (system "git push")))
+        (printf "path ~a does not exist.\n" draft-path))))
---
+++ render-post.rkt
@@ -0,0 +1,63 @@
+#lang racket/base
+(require racket/file
+         racket/list
+         racket/string)
+
+(provide render-post)
+
+(define (render-post path)
+  (define lines (file->lines path))
+  (define title (first lines))
+  (define keywords (string-split (second lines)))
+  (define body (string-join (rest (rest lines)) "\n"))
+  (format "\
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>~a</title>
+<link rel=\"stylesheet\" href=\"style.css\"/>
+</head>
+
+<body>
+<h1>~a</h1>
+<hr/>
+~a
+~a
+~a
+</body>
+
+</html>
+" title
+  title
+  body
+  (google-analytics-code)
+  (disqus-code title)))
+
+(define (google-analytics-code) "\
+<script type=\"text/javascript\">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-40150784-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>\
+")
+
+(define (disqus-code title)
+  (format "\
+<script type=\"text/javascript\">
+  var disqus_shortname = 'kimballgermanenet';
+  var disqus_id = '~a';
+
+ (function() {
+   var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+   dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+   (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+ })();
+</script>\
+" title))
